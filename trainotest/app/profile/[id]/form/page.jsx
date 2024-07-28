0d62e40a8329a62a2c46f480007cac4a1c9e7b0a
Fredrik Berglund <fredrik@diam.se>
2024-07-28 16:50:37 +0200

    Form object updates

--- trainotest/app/profile/[id]/form/page.jsx
+++ trainotest/app/profile/[id]/form/page.jsx
@@ -6,6 +6,7 @@ import "./page.css";
 export default function Form({ params }) {
     const [userinfo, setUserinfo] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [formData, setFormData] = useState({ title: "", type: "Tränare", description: "", truefalse: false });
 
     const id = params.id;
 
@@ -30,6 +31,50 @@ export default function Form({ params }) {
         fetchUserInfo();
     }, [id]);
 
+    const handleTitleInput = (event) => {
+        setFormData((prevFormData) => {
+            const newFormData = {
+                ...prevFormData,
+                title: event.target.value,
+            };
+            console.log("FormData:", newFormData);
+            return newFormData;
+        });
+    };
+
+    const handleTypeSelect = (event) => {
+        setFormData((prevFormData) => {
+            const newFormData = {
+                ...prevFormData,
+                type: event.target.value,
+            };
+            console.log("FormData:", newFormData);
+            return newFormData;
+        });
+    };
+
+    const handleTextArea = (event) => {
+        setFormData((prevFormData) => {
+            const newFormData = {
+                ...prevFormData,
+                description: event.target.value,
+            };
+            console.log("FormData:", newFormData);
+            return newFormData;
+        });
+    };
+
+    const handleCheckbox = (event) => {
+        setFormData((prevFormData) => {
+            const newFormData = {
+                ...prevFormData,
+                truefalse: event.target.checked,
+            };
+            console.log("FormData:", newFormData);
+            return newFormData;
+        });
+    };
+
     return (
         <>
             {loading ? (
@@ -42,8 +87,52 @@ export default function Form({ params }) {
                                 <Image src={userinfo.thumbnail} width="100" height="100" alt="" />
                             </div>
                             <div className="name">{`${userinfo.firstname} ${userinfo.lastname}`}</div>
+                            <div className="age">{userinfo.age}år</div>
                         </div>
                     )}
+
+                    <form>
+                        <div className="input-group">
+                            <label htmlFor="title">Titel</label>
+                            <input
+                                type="text"
+                                id="title"
+                                name="title"
+                                value={formData.title}
+                                onChange={handleTitleInput}
+                            />
+                        </div>
+
+                        <div className="input-group">
+                            <label htmlFor="type">Typ</label>
+                            <select id="type" name="type" value={formData.type} onChange={handleTypeSelect}>
+                                <option value="Tränare">Tränare</option>
+                                <option value="Trainee">Trainee</option>
+                            </select>
+                        </div>
+
+                        <div className="input-group">
+                            <label htmlFor="description">Beskrivning</label>
+                            <textarea
+                                name="description"
+                                id="description"
+                                value={formData.description}
+                                onChange={handleTextArea}
+                            ></textarea>
+                        </div>
+
+                        <div className="input-group">
+                            <input
+                                type="checkbox"
+                                name="truefalse"
+                                id="truefalse"
+                                value={formData.truefalse}
+                                onChange={handleCheckbox}
+                            />
+                        </div>
+
+                        <button>Skicka</button>
+                    </form>
                 </div>
             )}
         </>
